979f28d59911bd811fd5f29d6d2a398e963c8da6
samihatasnim0904 <samihatasnim0904@gmail.com>
2026-03-08 11:40:22 +0600

    Commit

--- Lab_Prac/JavaScript/Script.js
+++ Lab_Prac/JavaScript/Script.js
@@ -1,3 +1,25 @@
+function collect_name()
+{
+    let name = document.getElementById("fname").value;
+
+    if(name == "")
+    {
+        document.getElementById("NameError").innerHTML = "Name cannot be empty";
+        return false;
+    }
+    else if(name.length < 5)
+    {
+        document.getElementById("NameError").innerHTML = "Name must carry at least 5 characters";
+        return false;
+    }
+    else
+    {
+        document.getElementById("NameError").innerHTML = "";
+        return true;
+    }
+}
+
+
 document.getElementById("contactForm").addEventListener("submit", function(e){
 
     e.preventDefault();
@@ -8,7 +30,8 @@ document.getElementById("contactForm").addEventListener("submit", function(e){
     let phone = document.getElementById("phone").value;
     let message = document.getElementById("message").value;
 
-    if(fname=="" || lname=="" || email=="" || phone=="" || message==""){
+    if(fname=="" || lname=="" || email=="" || phone=="" || message=="")
+    {
         alert("Field Value need to be filled up");
         return;
     }
@@ -22,3 +45,31 @@ document.getElementById("contactForm").addEventListener("submit", function(e){
     alert("Form Submitted Successfully");
 
 });
+
+
+
+function collet_data()
+{
+    let isnamevalid= collect_name();
+    let isageValid = collect_age();
+// let name = document.getElementById("PatientName").value;
+// console.log(name);
+
+
+// let PatientAge = document.getElementById("Age").value;
+// console.log(PatientAge);
+
+// let DateOfBirth = document.getElementById("DOB").value;
+// console.log(DateOfBirth); 
+
+// let name = document.getElementById("PatientName");
+// var para1 = document.getElementById("demo");
+// para1.innerHTML="Patient Name: "+ name.value;
+
+// let age = document.getElementById("Age");
+// var para2 = document.getElementById("PAge");
+// para2.innerHTML= "Patient Age: " + age.value;
+
+
+return false;
+}
