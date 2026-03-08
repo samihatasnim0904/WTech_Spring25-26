95c3810e03cf841efa806e06e4840c98119a28ee
samihatasnim0904 <samihatasnim0904@gmail.com>
2026-03-08 11:47:57 +0600

    Commit

--- Lab_Prac/JavaScript/Script.js
+++ Lab_Prac/JavaScript/Script.js
@@ -52,23 +52,6 @@ function collet_data()
 {
     let isnamevalid= collect_name();
     let isageValid = collect_age();
-// let name = document.getElementById("PatientName").value;
-// console.log(name);
-
-
-// let PatientAge = document.getElementById("Age").value;
-// console.log(PatientAge);
-
-// let DateOfBirth = document.getElementById("DOB").value;
-// console.log(DateOfBirth); 
-
-// let name = document.getElementById("PatientName");
-// var para1 = document.getElementById("demo");
-// para1.innerHTML="Patient Name: "+ name.value;
-
-// let age = document.getElementById("Age");
-// var para2 = document.getElementById("PAge");
-// para2.innerHTML= "Patient Age: " + age.value;
 
 
 return false;
