6fb5d8517f7a7adab9af8ff7d09e4dcfe7375838
samihatasnim0904 <samihatasnim0904@gmail.com>
2026-04-12 11:12:15 +0600

    commit

---
+++ FINAL_Term/Lab1.php/View/Login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <form>
+            <?php
+            echo "<h1 style='color: red'> Login Page</h1>"
+            ?>
+            <table>
+                <tr>
+                    <td>User name:</td>
+                    <td><input type="text"></td>
+                </tr>
+                <tr>
+                    <td>Passweor:</td>
+                    <td><input type="text"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type="submit">
+                    </td>
+                </tr>
+
+            </table>
+        </form>
+        <?php
+            echo "<h4 style='color: red'> Login Introduction</h4>"
+            ?>
+    </body>
+</html>
